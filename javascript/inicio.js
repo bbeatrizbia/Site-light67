bcf516067fa8215a81f8a789ea593bb8d59c899f
Maria Beatriz <mariabeatrizs.fideles@gmail.com>
2024-09-08 15:10:49 -0300

    Atualização 1.7

--- javascript/inicio.js
+++ javascript/inicio.js
@@ -81,3 +81,125 @@ const elemento = document.querySelectorAll('.animacao')
 
 elemento.forEach((elemento) => myObserver.observe(elemento))
 //------Fim animação scroll------
+
+//------Início janela modal------
+function abrirmodal(){
+  const modal = document.getElementById('janela-modal')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal1(){
+  const modal = document.getElementById('janela-modal1')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal1'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal2(){
+  const modal = document.getElementById('janela-modal2')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal2'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal3(){
+  const modal = document.getElementById('janela-modal3')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal3'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal4(){
+  const modal = document.getElementById('janela-modal4')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal4'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal5(){
+  const modal = document.getElementById('janela-modal5')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal5'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal6(){
+  const modal = document.getElementById('janela-modal6')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal6'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal7(){
+  const modal = document.getElementById('janela-modal7')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal7'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal8(){
+  const modal = document.getElementById('janela-modal8')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal8'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+
+function abrirmodal9(){
+  const modal = document.getElementById('janela-modal9')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'fechar' || e.target.id == 'janela-modal9'){
+      modal.classList.remove('abrir')
+    }
+  })
+
+}
+//------Fim janela modal------
